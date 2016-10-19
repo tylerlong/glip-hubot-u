7f6a24fa34d5af53b87f1b7305a98bc7d5d7fc2c
Tyler Long <tyler4long@gmail.com>
2016-10-19 23:09:27 +0800

    Show review detail

--- scripts/appreviews.js
+++ scripts/appreviews.js
@@ -2,16 +2,29 @@ const request = require('request');
 const _ = require('lodash');
 
 module.exports = (robot) => {
+
   robot.respond(/reviews$/i, (res) => {
     request({ url: 'https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=715886894/json' }, (error, response, body) => {
       if (!error && response.statusCode == 200) {
         const json = JSON.parse(body);
         let result = `#.\tRating\tTitle\n`;
-        result += _.tail(json.feed.entry).map((item, index) => {
-          return `#${index + 1}.\t${item['im:rating'].label}\t${item.title.label}`;
+        result += _.tail(json.feed.entry).map((entry, index) => {
+          return `#${index + 1}.\t${'*'.repeat(entry['im:rating'].label)}\t${entry.title.label}`;
         }).join('\n');
         res.send(result);
       }
     });
   });
+
+  robot.respond(/reviews (\d+)$/i, (res) => {
+    request({ url: 'https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=715886894/json' }, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        const json = JSON.parse(body);
+        const entry = json.feed.entry[res.match[1]]
+        let result = `${'*'.repeat(entry['im:rating'].label)}\t${entry.title.label}\n\n${entry.content.label}`
+        res.send(result);
+      }
+    });
+  });
+
 }
