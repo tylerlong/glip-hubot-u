c6fcc1cd7c1b3feb144f8a621189660bb9e89770
Tyler Long <tyler4long@gmail.com>
2016-10-19 22:54:10 +0800

    First working version of appreviews

--- scripts/appreviews.js
+++ scripts/appreviews.js
@@ -1,11 +1,16 @@
 const request = require('request');
+const _ = require('lodash');
 
 module.exports = (robot) => {
   robot.respond(/reviews$/i, (res) => {
     request({ url: 'https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=715886894/json' }, (error, response, body) => {
       if (!error && response.statusCode == 200) {
         const json = JSON.parse(body);
-        res.send(json.feed.entry[1].title.label)
+        let result = `#.\tRating\tTitle\n`;
+        result += _.tail(json.feed.entry).map((item, index) => {
+          return `#${index + 1}.\t${item['im:rating'].label}\t${item.title.label}`;
+        }).join('\n');
+        res.send(result);
       }
     });
   });
