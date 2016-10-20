f1fc86ad702ec3b968b450cd2d360794b07aa184
Tyler Long <tyler4long@gmail.com>
2016-10-20 13:16:56 +0800

    Fix a typo

--- markdown.js
+++ markdown.js
@@ -1,11 +1,11 @@
 const uuid = require('uuid');
-const spawn = require('child_process').spawnSync
+const spawn = require('child_process').spawnSync;
 
 
 const send_markdown = (markdown, robot, res) => {
   const uid = uuid.v4();
   spawn('phantomjs', ['preview_markdown.js', markdown, uid]);
-  const url = `#{process.env.GLIP_X_HOST}/${uid}.png`;
+  const url = `#{process.env.GLIP_W_HOST}/${uid}.png`;
   const envelope = { user: res.message.user, message_type: 'image_url' };
   robot.send(envelope, url)
 }
