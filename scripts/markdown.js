9fbfad3c7ca07f1e35b9bc6f1d2b00d24541fe1d
Tyler Long <tyler4long@gmail.com>
2016-10-21 21:24:00 +0800

    Refactor markdown script

--- scripts/markdown.js
+++ scripts/markdown.js
@@ -1,9 +1,17 @@
+// Description:
+//   Render a line of markdown into image
+//
+// Commands:
+//   markdown <a line of markdown> - Render a line of markdown into image
+
+
+
 const { send_markdown } = require('../markdown');
 
 
 module.exports = (robot) => {
 
-  robot.respond(/markdown (.+?)$/i, { id: 'markdown' }, (res) => {
+  robot.hear(/^markdown (.+?)$/i, { id: 'markdown' }, (res) => {
     send_markdown(res.match[1], robot, res);
   });
 
