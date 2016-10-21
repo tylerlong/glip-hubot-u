a6cd2d1d75d31bcced8d4be7c54f4cc55576f526
Tyler Long <tyler4long@gmail.com>
2016-10-21 21:23:45 +0800

    Add glip_request method

---
+++ glip.js
@@ -0,0 +1,12 @@
+const glip_request = (robot, endpoint, method, options) => {
+  return new Promise((resolve, reject) => {
+    robot.adapter.client.request(endpoint, method, options, (error, pack) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(pack);
+    });
+  });
+};
+
+module.exports = { glip_request };
