332a7c331ac1222219e3eac0ce912e0e03cde03e
Tyler Long <tyler4long@gmail.com>
2016-10-22 09:15:52 +0800

    Make reviews accept app name or ID as parameter

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -2,16 +2,44 @@
 //   Show glip app reviews
 //
 // Commands:
-//   reviews <app name> - List recent 50 reviews of the specific app
+//   reviews <app name or ID> - List recent 50 reviews of the specific app
+//   reviews <app name or ID> <n> - Show detail of No. #n review of the specific app
 
 const request = require('request');
 const _ = require('lodash');
 
 
+const apps = {
+  glip: 715886894,
+  ringcentral: 293305984,
+  meetings: 688920955
+}
+
+
+const getApp = (res) => {
+  let app = _.toLower(res.match[1].trim());
+  if (!apps[app] && !app.match(/^(?:id)?\d+$/)) {
+    return null;
+  }
+  app = apps[app] || app;
+  if (_.startsWith(app, 'id')) {
+    app = app.substring(2);
+  }
+  return app;
+}
+
+
 module.exports = (robot) => {
 
-  robot.hear(/^reviews$/i, { id: 'reviews' }, (res) => {
-    request({ url: 'https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=715886894/json' }, (error, response, body) => {
+  robot.hear(/^reviews\s+([^\s]+?)$/i, { id: 'reviews' }, (res) => {
+    let app = getApp(res);
+    if (app == null) {
+      let message = `Non RingCentral apps (glip, ringcentral and meetings), specify app ID instead.`;
+      message += `\n\nFor example: Slack's ID is 803453959, HipChat's ID is 418168984...etc`;
+      res.send(message);
+      return;
+    }
+    request({ url: `https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=${app}/json` }, (error, response, body) => {
       if (!error && response.statusCode == 200) {
         const json = JSON.parse(body);
         let result = `#.\tRating\tTitle\n`;
@@ -23,11 +51,19 @@ module.exports = (robot) => {
     });
   });
 
-  robot.hear(/^reviews (\d+)$/i, { id: 'reviews' }, (res) => {
-    request({ url: 'https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=715886894/json' }, (error, response, body) => {
+  robot.hear(/^reviews\s+(.+?)\s+(\d+)$/i, { id: 'reviews' }, (res) => {
+    let app = getApp(res);
+    if (app == null) {
+      let message = `Non RingCentral apps (glip, ringcentral and meetings), specify app ID instead.`;
+      message += `\n\nFor example: Slack's ID is 803453959, HipChat's ID is 418168984...etc`;
+      res.send(message);
+      return;
+    }
+    let number = res.match[2];
+    request({ url: `https://itunes.apple.com/us/rss/customerreviews/page=1/sortBy=mostRecent/id=${app}/json` }, (error, response, body) => {
       if (!error && response.statusCode == 200) {
         const json = JSON.parse(body);
-        const entry = json.feed.entry[res.match[1]]
+        const entry = json.feed.entry[number]
         const result = `${':star:'.repeat(entry['im:rating'].label)}\t${entry.title.label}\n\n${entry.content.label}`
         res.send(result);
       }
