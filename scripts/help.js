57979c6a507685b6bbf5ae268240f04b37d60031
Tyler Long <tyler4long@gmail.com>
2016-10-22 08:48:15 +0800

    Use template to render help

--- scripts/help.js
+++ scripts/help.js
@@ -3,21 +3,22 @@
 
 const _ = require('lodash');
 const { getApps, getInstalledApps } = require('../apps');
+const { engine } = require('../nunjucks');
 
 
 module.exports = (robot) => {
   robot.hear(/^help$/i, { id: 'help' }, (res) => {
     const apps = getApps(robot);
     const installedApps = getInstalledApps(apps, robot, res);
-    console.log(installedApps);
-    const regexStr = '/^' + installedApps.join('\\s+|^') + '\\b/';
-    const regex = eval(regexStr);
-    let commands = robot.helpCommands();
-    console.log(commands);
-    commands = _.filter(commands, (command) => {
-      return command.match(regex) != null;
-    });
-    console.log(commands);
-    res.send(commands.join('\n'));
+    let commands = [];
+    if(installedApps.length > 0) {
+      const regexStr = '/^' + installedApps.join('\\s+|^') + '\\b/';
+      const regex = eval(regexStr);
+      commands = _.filter(robot.helpCommands(), (command) => {
+        return command.match(regex) != null;
+      });
+    }
+    const markdown = engine.render('help/list.njk', { commands });
+    res.send(markdown);
   });
 }
